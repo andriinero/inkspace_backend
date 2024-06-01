694b19b6eb223312c6625bdafe76af34263799f3
siriuszx <114768817+Siriuszx@users.noreply.github.com>
2024-06-01 12:29:11 +0200

    deps: update linter deps

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const passport = require('passport');
-const JWTStrategy = require('passport-jwt').Strategy;
-const ExtractJWT = require('passport-jwt').ExtractJwt;
-const compression = require('compression');
-const helmet = require('helmet');
-const rateLimit = require('express-rate-limit');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const passport = require("passport");
+const JWTStrategy = require("passport-jwt").Strategy;
+const ExtractJWT = require("passport-jwt").ExtractJwt;
+const compression = require("compression");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const User = require('./models/user');
+const User = require("./models/user");
 
 const strategyOpts = {
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
   secretOrKey: process.env.SECRET_KEY,
 };
 
-const indexRouter = require('./routes/index');
-const postRouter = require('./routes/postRouter');
-const commentRouter = require('./routes/commentRouter');
-const authorRouter = require('./routes/authorRouter');
-const topicRouter = require('./routes/topicRouter');
-const profileRouter = require('./routes/profileRouter');
-const authRouter = require('./routes/authRouter');
-const imageRouter = require('./routes/imageRouter');
+const indexRouter = require("./routes/index");
+const postRouter = require("./routes/postRouter");
+const commentRouter = require("./routes/commentRouter");
+const authorRouter = require("./routes/authorRouter");
+const topicRouter = require("./routes/topicRouter");
+const profileRouter = require("./routes/profileRouter");
+const authRouter = require("./routes/authRouter");
+const imageRouter = require("./routes/imageRouter");
 
 const app = express();
 
 const mongoDB = process.env.DEV_MONGODB_URI;
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 const main = async () => {
   await mongoose.connect(mongoDB);
 };
@@ -47,11 +47,11 @@ app.use(limiter);
 app.use(cors());
 app.use(helmet());
 app.use(compression());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 passport.use(
   new JWTStrategy(strategyOpts, async (jwt_payload, done) => {
@@ -66,16 +66,16 @@ passport.use(
     } catch (err) {
       return done(err, null);
     }
-  })
+  }),
 );
 
-app.use('/', indexRouter);
-app.use('/api/posts', postRouter);
-app.use('/api/comments', commentRouter);
-app.use('/api/authors', authorRouter);
-app.use('/api/topics', topicRouter);
-app.use('/api/profile', profileRouter);
-app.use('/auth', authRouter);
-app.use('/api/images', imageRouter);
+app.use("/", indexRouter);
+app.use("/api/posts", postRouter);
+app.use("/api/comments", commentRouter);
+app.use("/api/authors", authorRouter);
+app.use("/api/topics", topicRouter);
+app.use("/api/profile", profileRouter);
+app.use("/auth", authRouter);
+app.use("/api/images", imageRouter);
 
 module.exports = app;
